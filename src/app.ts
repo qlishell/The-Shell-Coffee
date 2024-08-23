0bdbdfd9941d5deed2f356c6a2d03700b8a33538
Qliphort Shell <hknguyen047@gmail.com>
2024-08-23 10:49:19 -0500

    prettier --write

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// Tailwind stylesheet
-import "css/tailwind.scss";
-
 // ZaUI stylesheet
 import "zmp-ui/zaui.css";
 
+// Tailwind stylesheet
+import "css/tailwind.scss";
 // Your stylesheet
 import "css/app.scss";
+
+// Style Swiper
+import "swiper/css";
+import "swiper/css/pagination";
 
 // Expose app configuration
 import appConfig from "../app-config.json";
--- src/state.ts
+++ src/state.ts
@@ -1,15 +1,18 @@
-import { atom, selector } from "recoil";
+import { selector } from "recoil";
 import { getUserInfo } from "zmp-sdk";
 
 export const userState = selector({
     key: "user",
-    get: () =>
-        getUserInfo({
-            avatarType: "normal",
-        }),
-});
-
-export const displayNameState = atom({
-    key: "displayName",
-    default: "",
+    get: async () => {
+        try {
+            const { userInfo } = await getUserInfo({ autoRequestPermission: true });
+            return userInfo;
+        } catch (error) {
+            return {
+                id: "",
+                avatar: "",
+                name: "Người dùng Zalo",
+            };
+        }
+    },
 });
