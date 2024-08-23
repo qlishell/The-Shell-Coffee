83e67a8ce38d80cf46a6285a66bd63c91a58396f
Qliphort Shell <hknguyen047@gmail.com>
2024-08-23 10:01:23 -0500

    getting started

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 const tailwindcss = require("tailwindcss");
 
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+    plugins: [require("postcss-aspect-ratio-polyfill"), tailwindcss("./tailwind.config.js"), require("autoprefixer")],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,25 @@
 import React, { Suspense } from "react";
-import { List, Page, Icon, useNavigate } from "zmp-ui";
-import UserCard from "components/user-card";
+import { Icon, List, Page, useNavigate } from "zmp-ui";
 
 const HomePage: React.FunctionComponent = () => {
-  const navigate = useNavigate();
-  return (
-    <Page className="page">
-      <Suspense>
-        <div className="section-container">
-          <UserCard />
-        </div>
-      </Suspense>
-      <div className="section-container">
-        <List>
-          <List.Item
-            onClick={() => navigate("/about")}
-            suffix={<Icon icon="zi-arrow-right" />}
-          >
-            <div>About</div>
-          </List.Item>
-          <List.Item
-            onClick={() => navigate("/user")}
-            suffix={<Icon icon="zi-arrow-right" />}
-          >
-            <div>User</div>
-          </List.Item>
-        </List>
-      </div>
-    </Page>
-  );
+    const navigate = useNavigate();
+    return (
+        <Page className="page">
+            <Suspense>
+                <div className="section-container">Hi, Nguyen Huu Hoang Kiet</div>
+            </Suspense>
+            <div className="section-container">
+                <List>
+                    <List.Item onClick={() => navigate("/about")} suffix={<Icon icon="zi-arrow-right" />}>
+                        <div>About</div>
+                    </List.Item>
+                    <List.Item onClick={() => navigate("/user")} suffix={<Icon icon="zi-arrow-right" />}>
+                        <div>User</div>
+                    </List.Item>
+                </List>
+            </div>
+        </Page>
+    );
 };
 
 export default HomePage;
