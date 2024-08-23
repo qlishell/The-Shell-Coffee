1a21f16a7a54ba972cae6b8a5e89d3bae658d121
Qliphort Shell <hknguyen047@gmail.com>
2024-08-23 12:17:52 -0500

    feat: home of categories

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,30 @@
+import { Divider } from "components/divider";
 import React, { Suspense } from "react";
-import { Icon, List, Page, useNavigate } from "zmp-ui";
+import { Box, Page, useNavigate } from "zmp-ui";
+import { Banner } from "./banner";
+import { Categories } from "./categories";
+import { Inquiry } from "./inquiry";
+import { ProductList } from "./product-list";
+import { Recommend } from "./recommend";
+import { Welcome } from "./welcome";
 
 const HomePage: React.FunctionComponent = () => {
     const navigate = useNavigate();
     return (
-        <Page className="page">
-            <Suspense>
-                <div className="section-container">Hi, Nguyen Huu Hoang Kiet</div>
-            </Suspense>
-            <div className="section-container">
-                <List>
-                    <List.Item onClick={() => navigate("/about")} suffix={<Icon icon="zi-arrow-right" />}>
-                        <div>About</div>
-                    </List.Item>
-                    <List.Item onClick={() => navigate("/user")} suffix={<Icon icon="zi-arrow-right" />}>
-                        <div>User</div>
-                    </List.Item>
-                </List>
-            </div>
+        <Page className="relative flex-1 flex flex-col bg-white">
+            <Welcome />
+            <Box className="flex-1 overflow-auto">
+                <Inquiry />
+                <Banner />
+                <Suspense>
+                    <Categories />
+                </Suspense>
+                <Divider />
+                <Recommend />
+                <Divider />
+                <ProductList />
+                <Divider />
+            </Box>
         </Page>
     );
 };
