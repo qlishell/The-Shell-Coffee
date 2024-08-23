35c8553a55a9e60e81bf6b70394028bfe424c78b
Qliphort Shell <hknguyen047@gmail.com>
2024-08-23 12:46:28 -0500

    product

---
+++ src/types/product.ts
@@ -0,0 +1,46 @@
+export interface PercentSale {
+    type: "percent";
+    percent: number;
+}
+
+export interface FixedSale {
+    amount: number;
+    type: "fixed";
+}
+
+export type Sale = PercentSale | FixedSale;
+
+export interface Option {
+    id: string;
+    label?: string;
+    priceChange?: Sale;
+}
+
+export interface BaseVariant {
+    id: string;
+    label?: string;
+    options: Option[];
+}
+
+export interface SingleOptionVariant extends BaseVariant {
+    type: "single";
+    default?: string;
+}
+
+export interface MultipleOptionVariant extends BaseVariant {
+    type: "multiple";
+    default?: string[];
+}
+
+export type Variant = SingleOptionVariant | MultipleOptionVariant;
+
+export interface Product {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+    categoryId: string[];
+    description?: string;
+    sale?: Sale;
+    variants?: Variant[];
+}
