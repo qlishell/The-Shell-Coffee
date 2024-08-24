545570c7375760ea68512823a67baabae89358c2
Qliphort Shell <hknguyen047@gmail.com>
2024-08-24 17:06:57 -0500

    index

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,18 +1,3 @@
-import { selector } from "recoil";
-import { getUserInfo } from "zmp-sdk";
+import { userState } from "./user.state";
 
-export const userState = selector({
-    key: "user",
-    get: async () => {
-        try {
-            const { userInfo } = await getUserInfo({ autoRequestPermission: true });
-            return userInfo;
-        } catch (error) {
-            return {
-                id: "",
-                avatar: "",
-                name: "Người dùng Zalo",
-            };
-        }
-    },
-});
+export { userState };
