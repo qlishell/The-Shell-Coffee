dc9631f7f40fc44994bdcf182a0b38c8dea20ca8
Qliphort Shell <hknguyen047@gmail.com>
2024-08-24 16:50:36 -0500

    app

---
+++ src/layout/app.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { App, SnackbarProvider, ZMPRouter } from "zmp-ui";
+import { Layout } from "./layout";
+
+const MyApp = () => {
+    return (
+        <RecoilRoot>
+            <App>
+                <SnackbarProvider>
+                    <ZMPRouter>
+                        <Layout />
+                    </ZMPRouter>
+                </SnackbarProvider>
+            </App>
+        </RecoilRoot>
+    );
+};
+export default MyApp;
