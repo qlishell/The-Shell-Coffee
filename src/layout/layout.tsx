3ceb0f291898d872ed9d4216938d725f014c0633
Qliphort Shell <hknguyen047@gmail.com>
2024-08-24 16:50:59 -0500

    layout

---
+++ src/layout/layout.tsx
@@ -0,0 +1,24 @@
+import HomePage from "pages/index";
+import React, { FC } from "react";
+import { Route, Routes } from "react-router";
+import { getSystemInfo } from "zmp-sdk";
+import { Box } from "zmp-ui";
+
+if (getSystemInfo().platform === "android") {
+    const androidSafeTop = Math.round(
+        (window as any).ZaloJavaScriptInterface.getStatusBarHeight() / window.devicePixelRatio,
+    );
+    document.body.style.setProperty("--zaui-safe-area-inset-top", `${androidSafeTop}px`);
+}
+
+export const Layout: FC = () => {
+    return (
+        <Box flex flexDirection="column" className="h-screen">
+            <Box className="flex-1 flex flex-col overflow-hidden">
+                <Routes>
+                    <Route path="/" element={<HomePage />}></Route>
+                </Routes>
+            </Box>
+        </Box>
+    );
+};
