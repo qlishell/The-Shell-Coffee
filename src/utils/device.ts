71bdc69ff0249b053b0fba78843b52edf3b66e7f
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:05:50 -0500

    docstring device

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,5 +1,10 @@
 import { configAppView } from "zmp-sdk";
 
+/**
+ * Khớp màu thanh trạng thái với khả năng hiển thị của một phần tử.
+ *
+ * @param {boolean} visible - Phần tử có hiển thị hay không.
+ */
 export function matchStatusBarColor(visible: boolean) {
     if (visible) {
         configAppView({
