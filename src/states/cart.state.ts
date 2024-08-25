a4c30848e19a73c282003a52115e9dd4b8361af4
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 17:37:24 -0500

    feat: totalPriceState and totalQuantityState

--- src/states/cart.state.ts
+++ src/states/cart.state.ts
@@ -1,6 +1,7 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 import { Cart } from "types/cart";
+import { calcFinalPrice } from "utils/product";
 
 /**
  * Recoil atom lưu trữ trạng thái giỏ hàng.
@@ -13,4 +14,30 @@ import { Cart } from "types/cart";
 export const cartState = atom<Cart>({
     key: "cart",
     default: [],
+});
+
+/**
+ * Tính toán tổng giá của các mặt hàng trong giỏ hàng.
+ *
+ * @returns {number} - Tổng giá các mặt hàng trong giỏ hàng.
+ */
+export const totalPriceState = selector({
+    key: "totalPrice",
+    get: ({ get }) => {
+        const cart = get(cartState);
+        return cart.reduce((total, item) => total + item.quantity * calcFinalPrice(item.product, item.options), 0);
+    },
+});
+
+/**
+ * Tính toán tổng số lượng mặt hàng trong giỏ hàng.
+ *
+ * @returns {number} - Tổng số lượng mặt hàng trong giỏ hàng.
+ */
+export const totalQuantityState = selector({
+    key: "totalQuantity",
+    get: ({ get }) => {
+        const cart = get(cartState);
+        return cart.reduce((total, item) => total + item.quantity, 0);
+    },
 });
