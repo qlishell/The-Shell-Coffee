715f2b1d95afb6bcdcfcaae943fb88c610e30d94
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 17:05:42 -0500

    cart screen

--- src/navigator/native-stack.tsx
+++ src/navigator/native-stack.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import { getSystemInfo } from "zmp-sdk";
 import { Box } from "zmp-ui";
 
+import CartPage from "screens/cart";
 import HomePage from "screens/index";
 import { Navigation } from "./bottom-tab";
 
@@ -18,7 +19,8 @@ export const NativeStack: FC = () => {
         <Box flex flexDirection="column" className="h-screen">
             <Box className="flex-1 flex flex-col overflow-hidden">
                 <Routes>
-                    <Route path="/" element={<HomePage />}></Route>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/cart" element={<CartPage />} />
                 </Routes>
             </Box>
             <Navigation />
