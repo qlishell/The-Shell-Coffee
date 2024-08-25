5bda8039d8ddfd6f51f551de46199d9509bf3c5b
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:56:25 -0500

    import: using ConfigProvider

--- src/layout/app.tsx
+++ src/layout/app.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import { App, SnackbarProvider, ZMPRouter } from "zmp-ui";
 
+import { ConfigProvider } from "components/config-provider";
 import { NativeStack } from "navigator/native-stack";
+import { getConfig } from "utils/config";
 
 const MyApp = () => {
     return (
         <RecoilRoot>
-            <App>
-                <SnackbarProvider>
-                    <ZMPRouter>
-                        <NativeStack />
-                    </ZMPRouter>
-                </SnackbarProvider>
-            </App>
+            <ConfigProvider
+                cssVariables={{
+                    "--zmp-primary-color": getConfig(c => c.template.primaryColor),
+                    "--zmp-background-color": "#f4f5f6",
+                }}
+            >
+                <App>
+                    <SnackbarProvider>
+                        <ZMPRouter>
+                            <NativeStack />
+                        </ZMPRouter>
+                    </SnackbarProvider>
+                </App>
+            </ConfigProvider>
         </RecoilRoot>
     );
 };
