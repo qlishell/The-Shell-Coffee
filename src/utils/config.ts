f1ef0104530db1bcdb0fd9c3e8a520c5e44070d0
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:05:21 -0500

    docstring config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,12 @@
 import appConfig from "../../app-config.json";
 
-export function getConfig<T>(getter: (config: typeof appConfig) => T) {
+/**
+ * Lấy một phần cấu hình từ đối tượng `appConfig`.
+ *
+ * @template T - Kiểu dữ liệu của giá trị được trả về.
+ * @param {((config: typeof appConfig) => T)} getter - Một hàm nhận đối tượng cấu hình và trả về giá trị mong muốn.
+ * @returns {T} - Giá trị cấu hình được trích xuất.
+ */
+export function getConfig<T>(getter: (config: typeof appConfig) => T): T {
     return getter(appConfig);
 }
