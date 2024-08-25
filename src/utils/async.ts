4ff8305181bbe6b999c895c5ccb0998850f30f95
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:04:34 -0500

    docstring async

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -1,3 +1,9 @@
-export function wait(ms: number) {
+/**
+ * Chờ trong một khoảng thời gian nhất định (đơn vị mili giây).
+ *
+ * @param {number} ms - Số mili giây cần chờ.
+ * @returns {Promise<void>} - Một Promise được giải quyết khi thời gian chờ kết thúc.
+ */
+export function wait(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
