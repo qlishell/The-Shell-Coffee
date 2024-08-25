07a3987900fb99c1a2738a030306faa8301981b4
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:26:46 -0500

    create new inquiry

---
+++ src/screens/index/inquiry.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+import { Box, Input, useNavigate } from "zmp-ui";
+
+export const Inquiry: FC = () => {
+    const navigate = useNavigate();
+    return (
+        <Box p={4} className="bg-white">
+            <Input.Search onFocus={() => navigate("/search")} placeholder="Tìm nhanh đồ uống, món mới ..." />
+        </Box>
+    );
+};
