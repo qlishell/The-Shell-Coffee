8b96245f1975a3a89eb421548902898c000ff432
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 14:06:27 -0500

    docstring getDummyImage

--- src/utils/product.ts
+++ src/utils/product.ts
@@ -1,3 +1,9 @@
-export function getDummyImage(filename: string) {
+/**
+ * Tạo URL cho hình ảnh giả từ tên tệp.
+ *
+ * @param {string} filename
+ * @returns {string}
+ */
+export function getDummyImage(filename: string): string {
     return `https://stc-zmp.zadn.vn/templates/zaui-coffee/dummy/${filename}`;
 }
