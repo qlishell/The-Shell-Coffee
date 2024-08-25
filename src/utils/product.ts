ad38c67d77d6e74f553943792c5d349efe397116
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 15:00:34 -0500

    feat: calcFinalPrice

--- src/utils/product.ts
+++ src/utils/product.ts
@@ -1,3 +1,6 @@
+import { SelectedOptions } from "types/cart";
+import { Option, Product } from "types/product";
+
 /**
  * Tạo URL cho hình ảnh giả từ tên tệp.
  *
@@ -6,4 +9,45 @@
  */
 export function getDummyImage(filename: string): string {
     return `https://stc-zmp.zadn.vn/templates/zaui-coffee/dummy/${filename}`;
+}
+
+export function calcFinalPrice(product: Product, options?: SelectedOptions) {
+    let finalPrice = product.price;
+    if (product.sale) {
+        if (product.sale.type === "fixed") {
+            finalPrice = product.price - product.sale.amount;
+        } else {
+            finalPrice = product.price * (1 - product.sale.percent);
+        }
+    }
+
+    if (options && product.variants) {
+        const selectedOptions: Option[] = [];
+        for (const variantKey in options) {
+            const variant = product.variants.find(v => v.id === variantKey);
+            if (variant) {
+                const currentOption = options[variantKey];
+                if (typeof currentOption === "string") {
+                    const selected = variant.options.find(o => o.id === currentOption);
+                    if (selected) {
+                        selectedOptions.push(selected);
+                    }
+                } else {
+                    const selecteds = variant.options.filter(o => currentOption.includes(o.id));
+                    selectedOptions.push(...selecteds);
+                }
+            }
+        }
+        finalPrice = selectedOptions.reduce((price, option) => {
+            if (option.priceChange) {
+                if (option.priceChange.type == "fixed") {
+                    return price + option.priceChange.amount;
+                } else {
+                    return price + product.price * option.priceChange.percent;
+                }
+            }
+            return price;
+        }, finalPrice);
+    }
+    return finalPrice;
 }
