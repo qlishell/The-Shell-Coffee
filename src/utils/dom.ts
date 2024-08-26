eee096198fdb29951f8f772599479d79b8253806
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 21:12:24 -0500

    docstring dom

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,3 +1,12 @@
+/**
+ * Lấy giá trị số từ một chuỗi.
+ *
+ * Hàm này sử dụng biểu thức chính quy để tìm một hoặc nhiều chữ số trong chuỗi đã cho.
+ * Giá trị tìm được sau đó được chuyển đổi thành số và trả về.
+ *
+ * @param {string} value - Chuỗi đầu vào.
+ * @returns {number} Giá trị số đã được lấy ra, hoặc `NaN` nếu không tìm thấy chữ số nào.
+ */
 export function tripUnit(value: string): number {
     return Number(value.match(/\d+/));
 }
