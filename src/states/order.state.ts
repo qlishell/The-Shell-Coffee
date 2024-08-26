46fe5e97ae27e9283c9ebc9b906bdfc3648742c1
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 21:06:34 -0500

    docstring order state

--- src/states/order.state.ts
+++ src/states/order.state.ts
@@ -39,6 +39,12 @@ export const selectedStoreState = selector({
     },
 });
 
+/**
+ * Truy xuất và lọc các cửa hàng lân cận dựa trên vị trí hiện tại.
+ *
+ * @returns {Promise<Store[]>} - Một Promise thành :
+ * * Một mảng các đối tượng `Store` đại diện cho các cửa hàng lân cận, được sắp xếp theo khoảng cách (gần nhất trước tiên).
+ */
 export const nearbyStoresState = selector({
     key: "nearbyStores",
     get: ({ get }) => {
@@ -64,6 +70,17 @@ export const nearbyStoresState = selector({
     },
 });
 
+/**
+ * Truy xuất và trả về vị trí hiện tại của người dùng.
+ *
+ * **Ghi chú quan trọng:**
+ * - selector này ưu tiên quyền riêng tư của người dùng và chỉ truy xuất dữ liệu vị trí khi có sự cho phép rõ ràng của người dùng.
+ * - Vị trí dự phòng chỉ nên được sử dụng để phát triển và không được đưa vào các bản dựng sản xuất.
+ *
+ * @returns {Promise<object | false>} - Một Promise:
+ * * Một đối tượng có thuộc tính `latitude` (chuỗi) và `kinh độ` (chuỗi) đại diện cho vị trí của người dùng (nếu thành công).
+ * * `false` nếu truy xuất vị trí không thành công hoặc người dùng chưa cấp quyền truy cập.
+ */
 export const locationState = selector<{ latitude: string; longitude: string } | false>({
     key: "location",
     get: async ({ get }) => {
