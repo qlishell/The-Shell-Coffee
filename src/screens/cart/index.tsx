d78165921a43c45b59b3929dc304f27ea9246983
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 19:34:19 -0500

    cart screen

--- src/screens/cart/index.tsx
+++ src/screens/cart/index.tsx
@@ -2,14 +2,24 @@ import React, { FC } from "react";
 import { Header, Page } from "zmp-ui";
 
 import { Divider } from "components/UI/divider";
+import { useVirtualKeyboardVisible } from "hooks";
 import { CartItems } from "./cart-items";
+import { Delivery } from "./delivery";
+import { CartPreview } from "./preview";
+import { TermsAndPolicies } from "./term-and-policies";
 
 const CartPage: FC = () => {
+    const keyboardVisible = useVirtualKeyboardVisible();
+
     return (
         <Page className="flex flex-col">
             <Header title="Giỏ hàng" showBackIcon={false} />
             <CartItems />
+            <Delivery />
             <Divider size={12} />
+            <TermsAndPolicies />
+            <Divider size={32} className="flex-1" />
+            {!keyboardVisible && <CartPreview />}
         </Page>
     );
 };
