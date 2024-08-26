e246e806c2eb5ef8df801941e8beaac9104c0367
Qliphort Shell <hknguyen047@gmail.com>
2024-08-25 19:20:20 -0500

    feat: fullscreen ActionSheet

--- src/components/UI/fullscreen-sheet.tsx
+++ src/components/UI/fullscreen-sheet.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from "react";
 import { Sheet as OrginalSheet } from "zmp-ui";
-import { SheetProps } from "zmp-ui/sheet";
+import { ActionSheetProps, SheetProps } from "zmp-ui/sheet";
 
 import { useMatchStatusTextColor } from "hooks";
 
 export const Sheet: FC<Omit<SheetProps, "ref">> = props => {
     useMatchStatusTextColor(props.visible);
     return <OrginalSheet {...props} />;
+};
+
+export const ActionSheet: FC<Omit<ActionSheetProps, "ref">> = props => {
+    useMatchStatusTextColor(props.visible);
+    return <OrginalSheet.Actions {...props} />;
 };
